2b1272159db34307fdd694c33f35a086e6c722ab
Yagth <yabuman46@gmail.com>
2023-02-23 16:14:34 +0300

    Learned higher order funcs

---
+++ GettingStarted/higherOrderFuncs.hs
@@ -0,0 +1,17 @@
+-- let maximumOfFive = (max 5)
+multthree :: (Num a) => a->a->a->a
+multthree x y z = x*y*z
+
+multWithFive :: (Num a) => a->a->a
+multWithFive = multthree 5
+
+multWithTwenty :: (Num a) => a->a
+multWithTwenty = multWithFive 4
+
+compareWithHundrud :: (Num a, Ord a) => a->Ordering
+compareWithHundrud = (`compare` 100)
+
+zipWith' :: (a->b->c)->[a]->[b]->[c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
