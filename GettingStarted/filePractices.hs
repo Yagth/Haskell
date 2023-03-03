75265c78a308d49271b589f018bf288fc5579550
Yagth <yabuman46@gmail.com>
2023-03-03 10:14:16 +0300

    Changed the import needed

--- GettingStarted/filePractices.hs
+++ GettingStarted/filePractices.hs
@@ -1,18 +1,15 @@
-import GHC.IO.Handle.FD
-import System.Directory.Internal.Prelude
-import Data.Text.IO
-import Control.Exception (handle)
+import System.IO
 
--- main = do
---     handle <- openFile "girlfriend.txt" ReadMode
---     contents <- hGetContents handle
---     Data.Text.IO.putStr contents
---     hClose handle
+main = do
+    handle <- openFile "girlfriend.txt" ReadMode
+    contents <- hGetContents handle
+    putStr contents
+    hClose handle
 
 main = do
     withFile' "girlfriend.txt" ReadMode (\handle -> do
         contents <- hGetContents handle
-        Data.Text.IO.putStr contents)
+        putStr contents)
 
 withFile' :: FilePath -> IOMode -> (Handle -> IO a) -> IO a
 withFile' path mode f = do
