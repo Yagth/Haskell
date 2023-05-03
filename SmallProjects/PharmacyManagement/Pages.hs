6e9cb74c079b788e814a4e97011e67489f5eca2c
Yagth <yabuman46@gmail.com>
2023-05-03 20:44:35 +0300

    Working on Parser Applicative instance

--- SmallProjects/PharmacyManagement/Pages.hs
+++ SmallProjects/PharmacyManagement/Pages.hs
@@ -3,6 +3,25 @@ module Pages where
 import Control.Applicative ( Alternative(many) )
 import Datatypes ( User, Password, Username )
 
+newtype Parser a = Parser {runParser :: String -> Maybe (String, a)}
+
+instance Functor Parser where
+    fmap :: (a -> b) -> Parser a -> Parser b
+    fmap f (Parser p1) = Parser (\input -> do
+            (rest, output) <- p1 input
+            Just (rest, f output)
+        )
+
+instance Applicative Parser where
+    pure :: a -> Parser a
+    pure = undefined
+    
+    (<*>) :: Parser (a -> b) -> Parser a -> Parser b
+    (<*>) (Parser f) (Parser p1) = Parser (\ input -> do
+        (rest, output) <- f input
+        (rest', output') <- p1 output
+        )
+
 loginPage :: IO ()
 loginPage = do
     putStrLn "****Pharma Login*****\n"
@@ -20,7 +39,3 @@ userPage = undefined
 
 findUserInFile :: Username -> Password -> IO (Maybe User)
 findUserInFile uname pass = undefined
-
-parse :: Char -> String -> [String]
-parse sep line = many (separate line)
-    where separate = takeWhile (/= sep)
