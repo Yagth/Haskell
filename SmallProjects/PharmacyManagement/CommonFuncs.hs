71f90ca8cfea3fb8135a34cd96251545d31d477c
Yagth <yabuman46@gmail.com>
2023-05-05 13:01:53 +0300

    Wrote an edit function for meds

--- SmallProjects/PharmacyManagement/CommonFuncs.hs
+++ SmallProjects/PharmacyManagement/CommonFuncs.hs
@@ -1,7 +1,10 @@
 module CommonFuncs where
 
 import Datatypes
+import Data.List
 import Parser
+import System.IO
+import Control.Monad (when, unless)
 
 userFile :: FilePath
 userFile = "DB/Users.txt"
@@ -21,18 +24,40 @@ getUsers filePath = do
     let users = sequence . filter (/= Nothing) $ fmap snd <$> map (runParser parseUser) (lines strUsers)
     return users
 
-findUsers :: String -> IO (Maybe User)
+findUsers :: Username -> IO (Maybe User)
 findUsers username = do
     (Just users) <- getUsers userFile
     let userLookUpTable = map (\user -> (userName user, user)) users
         foundUser       = lookup username userLookUpTable
     return foundUser
 
-findMeds :: String -> IO (Maybe Med)
+findMeds :: Name -> IO (Maybe Med)
 findMeds medName = do
     (Just meds) <- getMeds medFile
     let medLookupTable = map (\med -> (name med, med)) meds
         foundMed       = lookup medName medLookupTable
 
     return foundMed
+
+editMed  :: Name -> Med -> IO (Maybe Med)
+editMed medName newMed = do
+    handle <- openFile medFile ReadWriteMode
+
+    let loop :: IO (Maybe Med)
+        loop = do
+            eof <- hIsEOF handle
+            if eof
+                then return Nothing
+                else do
+                    line <- hGetLine handle
+                    let med = find (== medName ) (words line)
+                        newLine = case med of
+                         Just name -> show newMed
+                         _         -> line
+                        found = newLine /= line
+                    if found
+                        then return (Just newMed)
+                        else loop  
+    loop
+
     
