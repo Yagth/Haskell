ceb319a3a2d263df53f7ee5028967877f85f14fd
Yagth <yabuman46@gmail.com>
2023-05-05 12:08:54 +0300

    added searching funcs for meds and user

--- SmallProjects/PharmacyManagement/CommonFuncs.hs
+++ SmallProjects/PharmacyManagement/CommonFuncs.hs
@@ -20,3 +20,19 @@ getUsers filePath = do
     strUsers <- readFile filePath
     let users = sequence . filter (/= Nothing) $ fmap snd <$> map (runParser parseUser) (lines strUsers)
     return users
+
+findUsers :: String -> IO (Maybe User)
+findUsers username = do
+    (Just users) <- getUsers userFile
+    let userLookUpTable = map (\user -> (userName user, user)) users
+        foundUser       = lookup username userLookUpTable
+    return foundUser
+
+findMeds :: String -> IO (Maybe Med)
+findMeds medName = do
+    (Just meds) <- getMeds medFile
+    let medLookupTable = map (\med -> (name med, med)) meds
+        foundMed       = lookup medName medLookupTable
+
+    return foundMed
+    
